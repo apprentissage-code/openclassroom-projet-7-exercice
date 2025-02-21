3d201c9dcd7c691362eebfbf7d29ebb4f499e9a2
Solene Baudic <solene.baudic@gmail.com>
2025-02-21 10:25:22 +0100

    Feat create page show car

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Car;
 use App\Repository\CarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +10,25 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class CarController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(CarRepository $carRepository):Response
-    {
-      $cars = $carRepository->findAll();
+  #[Route('/', name: 'app_home')]
+  public function index(CarRepository $carRepository): Response
+  {
+    $cars = $carRepository->findAll();
 
-      return $this->render('car/index.html.twig', [
-          'cars' => $cars,
-      ]);
+    return $this->render('car/index.html.twig', [
+      'cars' => $cars,
+    ]);
+  }
+
+  #[Route('/car/{id}', name: 'app_car_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+  public function show(?Car $car): Response
+  {
+    if (!$car) {
+      return $this->redirectToRoute('app_home');
     }
+
+    return $this->render('car/car.html.twig', [
+      'car' => $car,
+    ]);
+  }
 }
